03d9b8469ef610d1a7236401c9ec582a7a878ff7
Alan <alan.fernandez.7e7@itb.cat>
2024-03-11 18:27:22 +0100

    Update script-rundocker.sh

--- Assets/A06 - Componentes desactualizados/script-rundocker.sh
+++ Assets/A06 - Componentes desactualizados/script-rundocker.sh
@@ -1,4 +1,4 @@
 # START MAQUINA 1 - CTF TU1MILLON 
 #!/bin/bash
 
-sudo docker run -d -e DISPLAY=:0.0 -v /tmp/.X11-unix:/tmp/.X11-unix -p 80:80 -p 22:22 --name tu1millon lladosimg
+sudo docker run -d -e DISPLAY=:0.0 -v /tmp/.X11-unix:/tmp/.X11-unix -p 80:80 -p 22:22 -p 2121:21 -p 2020:20 --name tu1millon lladosimg
